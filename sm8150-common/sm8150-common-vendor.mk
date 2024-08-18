2f097a10e78f1eac3d814814f8b61fa09c643f96
Alexander Koskovich <akoskovich@pm.me>
2024-08-19 01:08:10 +0200

    sm8150-common: Drop updatable GPU drivers
     * Motorola never wrote their own app so QCOM's is just
       a stub and this does nothing.
    
    Change-Id: Ia9e01ac6f8385741f225e355ec32cf615191baf5

--- sm8150-common/sm8150-common-vendor.mk
+++ sm8150-common/sm8150-common-vendor.mk
@@ -929,7 +929,6 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.msmnile.api30 \
     PowerOffAlarm \
     MotoSignatureApp \
     PowerOffAlarm \
